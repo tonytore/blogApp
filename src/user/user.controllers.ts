54d3f10141a501f1f3f544a64c8f44bd5f4b0255
tonytor <teme4842@gmail.com>
2025-11-14 17:19:10 +0300

    Enhance user management: add user existence check in update and create functions, and introduce prisma reset command in package.json

--- src/user/user.controllers.ts
+++ src/user/user.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import * as svc from '../user/user.service'
+import { db } from '../../prisma/db'
 
 const userControllers = {
   listUser : async (req:Request, res:Response) => {
@@ -25,6 +26,13 @@ updateUser : async (req:Request, res:Response) => {
   const {id} = req.params
   const {email,password,name,bio,avatarUrl} = req.body
   try {
+    const existingUser = await db.user.findUnique({where:{id}})
+    if(!existingUser){
+      return res.status(404).send({
+        data: null,
+        mas: "user not found"
+      })
+    }
     const user = await svc.updateUserService({id,email,password,name,bio,avatarUrl})
     const {password:_pass, ...other} = user
   return res.status(200).json({
